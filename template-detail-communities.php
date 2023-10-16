a248d08b63430d02c4a9403b5169badcbc702ed7
David Hund <david.hund@gmail.com>
2023-10-16 15:24:26 +0200

    WIP: highlighted needed MetaBoxes/Blocks

--- template-detail-communities.php
+++ template-detail-communities.php
@@ -18,8 +18,8 @@ $context['modifier']      = 'community-detail';
 $context['is_unboxed']   = true;
 
 if ( $current_community_term && ! is_wp_error( $current_community_term ) ) {
-    // Set custom page class
-    $context['pageclass'] = 'community--' . $current_community_term->slug;
+	// Set custom page class
+	$context['pageclass'] = 'community--' . $current_community_term->slug;
 }
 
 $templates               = [ 'community-detail.twig', 'page.twig' ];
@@ -57,30 +57,64 @@ if ( function_exists( 'gc_fill_context_with_acf_fields' ) ) {
 }
 
 // DEBUG: see custom ACF fields to this WP_Term..
-$current_community_term_fields = get_fields( $current_community_term );
-if ( $current_community_term_fields ) {
-    dump( $current_community_term_fields );
-}
+// $current_community_term_fields = get_fields( $current_community_term );
+// if ( $current_community_term_fields ) {
+// 	dump( $current_community_term_fields );
+// }
 
 
 if ( $current_community_term && ! is_wp_error( $current_community_term ) ) {
 
-	// move content from editor to metabox_freehandblocks
-	$blocks = parse_blocks( $timber_post->post_content );
-	foreach ( $blocks as $block ) {
-		if ( isset( $block['blockName'] ) && $block['blockName'] !== null ) {
-			$context['metabox_content'] = ( $context['metabox_content'] ?? '' ) . render_block( $block );
-		}
-	}
+	// // move content from editor to metabox_freehandblocks
+	// $blocks = parse_blocks( $timber_post->post_content );
+	// foreach ( $blocks as $block ) {
+	// 	if ( isset( $block['blockName'] ) && $block['blockName'] !== null ) {
+	// 		$context['metabox_content'] = ( $context['metabox_content'] ?? '' ) . render_block( $block );
+	// 	}
+	// }
 
 	// Page title is taken from term name
 	$timber_post->post_title = $current_community_term->name;
 
 	// text for 'inleiding' is taken from term description
 	// $timber_post->post_content = $current_community_term->description;
-    
-    // Use Intro instead?!
-    $context['intro'] = $current_community_term->description;
+	
+	// Use Intro instead?!
+	$context['intro'] = wpautop( $current_community_term->description );
+
+	/**
+	 *  Events box
+	 * ----------------------------- */
+	 // Only show events if Events Manager plugin is active
+	if ( class_exists( 'EM_Events' ) ) {
+
+		// // TEST: render block pattern
+		// // Challenge: how to fill in custom data?
+		// $agenda_pattern_name = 'gc/section-agenda';
+
+		// if ( class_exists( 'WP_Block_Patterns_Registry' ) ) {
+
+		// 	$block_pattern_registry = WP_Block_Patterns_Registry::get_instance();
+
+		// 	if ( $block_pattern_registry ) {
+		// 		$agenda_section_pattern = $block_pattern_registry->get_registered( $agenda_pattern_name );
+		// 		if ( $agenda_section_pattern ) {
+		// 			// Add our pattern HTML to our metabox_content context
+		// 			$context['metabox_content'] = ( $context['metabox_content'] ?? '' ) . do_blocks( $agenda_section_pattern['content'] );
+
+		// 			// dump(
+		// 			// 	$agenda_section_pattern
+		// 			// 	// do_blocks( $agenda_section_pattern['content'] )
+		// 			// 	// $agenda_section_pattern,
+		// 			// 	// $parsed_agenda_section_pattern,
+		// 			// 	// render_block( $parsed_agenda_section_pattern )
+		// 			// );
+		// 		}
+		// 	}
+	
+		// }
+
+	}
 
 }
 
