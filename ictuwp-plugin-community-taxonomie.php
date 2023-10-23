7b2ff457a3ad7807b44cb7822c5f3c7954cd1895
David Hund <david.hund@gmail.com>
2023-10-23 12:46:02 +0200

    version: 1.1.0 => 1.2.0

--- ictuwp-plugin-community-taxonomie.php
+++ ictuwp-plugin-community-taxonomie.php
@@ -8,8 +8,8 @@
  * Plugin Name:         ICTU / Gebruiker Centraal / Community taxonomie
  * Plugin URI:          https://github.com/ICTU/ictuwp-plugin-community-taxonomie
  * Description:         Plugin voor het aanmaken van de 'community'-taxonomie
- * Version:             1.1.0
- * Version description: Add extra ACF fields: Colorscheme, Visual & Link
+ * Version:             1.2.0
+ * Version description: Add extra ACF field: Contactform (Gravity)
  * Author:              David Hund
  * Author URI:          https://github.com/ICTU/ictuwp-plugin-community-taxonomie/
  * License:             GPL-3.0+
