cade49ee87ebae5e40ff857ddcc451957b4e0651
David Hund <david.hund@gmail.com>
2023-11-24 11:13:09 +0100

    fix: no longer need to append 'icon-' to modifier

--- template-detail-communities.php
+++ template-detail-communities.php
@@ -136,7 +136,7 @@ if ( $current_community_term && ! is_wp_error( $current_community_term ) ) {
 
 				$item = array();
 				if ( isset( $block['title'] ) && $block['title'] && isset( $block['description'] ) && $block['description'] ) {
-					$item['modifier']                            = 'icon-' . $block['icon'];
+					$item['modifier']                            = $block['icon'];
 					$item['title']                              = $block['title'];
 					$item['content']                            = wpautop( $block['description'] );
 					$context['metabox_icoonblokken']['items'][] = $item;
