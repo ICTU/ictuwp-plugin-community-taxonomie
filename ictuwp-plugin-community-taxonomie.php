91ab906c98ddb5ef55bf4e37ad487aa9b6692c7f
David Hund <david.hund@gmail.com>
2024-01-08 15:50:51 +0100

    update version to 1.5.5

--- ictuwp-plugin-community-taxonomie.php
+++ ictuwp-plugin-community-taxonomie.php
@@ -8,8 +8,8 @@
  * Plugin Name:         ICTU / Gebruiker Centraal / Community taxonomie
  * Plugin URI:          https://github.com/ICTU/ictuwp-plugin-community-taxonomie
  * Description:         Plugin voor het aanmaken van de 'community'-taxonomie
- * Version:             1.5.4
- * Version description: Added Partner (logo's) block in detail template.
+ * Version:             1.5.5
+ * Version description: Added category selection in automatic posts metabox.
  * Author:              David Hund
  * Author URI:          https://github.com/ICTU/ictuwp-plugin-community-taxonomie/
  * License:             GPL-3.0+
